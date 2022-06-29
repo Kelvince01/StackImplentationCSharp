967d1cfdc0e9925924f39c8487009510e582b719
kelvince pericles <kelvince05@outlook.com>
2022-06-29 08:53:40 +0300

    Initial commit

---
+++ StackImpl/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+
+namespace StackImpl
+{
+    internal class Program
+    {
+        private static Stack _stack;
+        
+        public static void Main(string[] args)
+        {
+            //System.Collections.Stack
+            _stack = new Stack();
+            _stack.Push(10);
+            _stack.Push(20);
+            _stack.Push(30);
+            _stack.Pop();
+            _stack.IsEmpty();
+        }
+    }
+}
---
+++ StackImpl/Stack.cs
@@ -0,0 +1,30 @@
+﻿namespace StackImpl
+{
+    public class Stack
+    {
+        public Stack()
+        {
+            
+        }
+        
+        public void Pop()
+        {
+            
+        }
+
+        public void Push(object i)
+        {
+            
+        }
+
+        public bool IsEmpty()
+        {
+            return true;
+        }
+
+        public int Count()
+        {
+            return 0;
+        }
+    }
+}
