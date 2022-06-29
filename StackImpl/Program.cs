6f71aa2700b312ce3a6353c14d197dd13259b6af
kelvince pericles <kelvince05@outlook.com>
2022-06-29 11:51:59 +0300

    Added stack implementation from scratch and sample usages

--- StackImpl/Program.cs
+++ StackImpl/Program.cs
@@ -1,20 +1,52 @@
-﻿using System.Collections;
+﻿using System;
 
 namespace StackImpl
 {
     internal class Program
     {
-        private static Stack _stack;
+        private static Stack<int> _stack;
+        private static int _capacity;
         
+        /// <summary>
+        /// Usage: CUSTOM STACK pop, push, peek, count and isempty
+        /// </summary>
+        /// <param name="args"></param>
         public static void Main(string[] args)
         {
-            //System.Collections.Stack
-            _stack = new Stack();
-            _stack.Push(10);
-            _stack.Push(20);
-            _stack.Push(30);
-            _stack.Pop();
-            _stack.IsEmpty();
+            _capacity = 100;
+            // Initialize stack with size: 100
+            _stack = new Stack<int>(_capacity);
+            
+            // Check if stack empty, OUTPUT -> TRUE
+            Console.WriteLine(_stack.IsEmpty());
+
+            // Add elements of type INT to stack
+            for (int i = 0; i < _capacity; i++)
+            {
+                _stack.Push(i);
+            }
+            
+            // Count pushed elements, OUTPUT -> 100
+            Console.WriteLine("Number of element in stack : {0}", _stack.Count());
+            // Check top element, OUTPUT -> 99
+            Console.WriteLine("Top of element in stack : {0}", _stack.Peek());
+            
+            // Display to console stack elements with Indexes, 100 of them
+            _stack.Display();
+            
+            // Pop 90 items from Stack
+            for (int i = 0; i < _capacity - 10; i++)
+            {
+                _stack.Pop();
+            }
+            
+            _stack.Display();
+            
+            Console.WriteLine("Number of element in stack : {0}", _stack.Count());
+            Console.WriteLine("Top of element in stack : {0}", _stack.Peek());
+            Console.WriteLine(_stack.Contains(10));
+
+            Console.WriteLine(_stack.IsEmpty());
         }
     }
 }
--- StackImpl/Stack.cs
+++ StackImpl/Stack.cs
@@ -1,30 +1,142 @@
-﻿namespace StackImpl
+﻿using System;
+
+namespace StackImpl
 {
-    public class Stack
+    /// <summary>
+    /// Linear Data Structure, uses Last In First Out(LIFO) Principle
+    /// Implemented methods: POP(), PUSH(T value), PEEK(), COUNT(), CONTAINS(T element) and CLEAR()
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class Stack<T>
     {
+        private T[] _stack;
+        private int _size;
+        private int _topIndex = -1;
+        
         public Stack()
         {
-            
+            this._stack = new T[10];
+            this._size = 0;
         }
         
-        public void Pop()
+        public Stack(int size)
         {
-            
+            _size = size;
+            _stack = new T[size];
+        }
+        
+        /// <summary>
+        /// Removes the last pushed Item from the Stack
+        /// </summary>
+        /// <returns></returns>
+        /// <exception cref="Exception"></exception>
+        public T Pop()
+        {
+            if (IsEmpty())
+            {
+                throw new Exception("Stack is Empty");
+            }
+
+            return _stack[_topIndex--];
         }
 
-        public void Push(object i)
+        /// <summary>
+        /// Adds element to the top of Stack
+        /// </summary>
+        /// <param name="i"></param>
+        /// <exception cref="Exception"></exception>
+        public void Push(T i)
         {
-            
+            if (IsFull())
+            {
+                throw new Exception("Stack overflow");
+            }
+
+            _stack[++_topIndex] = i;
+        }
+        
+        /// <summary>
+        /// Gets the top index element without removing it from the Stack
+        /// </summary>
+        /// <returns></returns>
+        /// <exception cref="Exception"></exception>
+        public T Peek()
+        {
+            if (IsEmpty())
+            {
+                throw new Exception("Stack is Empty");
+            }
+
+            return _stack[_topIndex];
+        }
+        
+        /// <summary>
+        /// Checks if Stack contains <see cref="element"/> provided
+        /// </summary>
+        /// <param name="element"></param>
+        /// <returns></returns>
+        public bool Contains(T element)
+        {
+            int size = this._size;
+            while (size-- > 0)
+            {
+                if (element == null)
+                {
+                    if (this._stack[size] == null)
+                        return true;
+                }
+                else if (this._stack[size] != null && this._stack[size].Equals(element))
+                    return true;
+            }
+            return false;
         }
 
-        public bool IsEmpty()
+        /// <summary>
+        /// Display all elements of the Stack
+        /// </summary>
+        public void Display()
         {
-            return true;
+            for (int i = _topIndex; i > -1; i--)
+            {
+                Console.WriteLine("Item {0}: {1}", (i + 1), _stack[i]);
+            }
         }
 
+        /// <summary>
+        /// Checks if stack contains any element
+        /// </summary>
+        /// <returns></returns>
+        public bool IsEmpty() => _topIndex == -1;
+
+        /// <summary>
+        /// Checks for a stack overflow
+        /// </summary>
+        /// <returns></returns>
+        public bool IsFull() => _size == _topIndex + 1;
+
+        /// <summary>
+        /// Counts the size of the Stack
+        /// </summary>
+        /// <returns></returns>
         public int Count()
         {
-            return 0;
+            _size = 0;
+            for (int i = _topIndex; i > -1; i--)
+            {
+                _size++;
+            }
+
+            return _size;
+        }
+
+        /// <summary>
+        /// Clears the Stack to default
+        /// </summary>
+        public void Clear()
+        {
+            _topIndex = -1;
+            _size = 0;
+            _stack = new T[10];
         }
     }
 }
